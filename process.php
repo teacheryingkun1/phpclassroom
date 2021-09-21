a2afb65941eb5cf39a0c664a4220ac2df005ab4d
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-09-21 13:51:15 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,13 +1,3 @@
 <?php
-  
-  $username =  $_GET['data1'];
-  $password =  $_GET['data2'];
-
-  if($username == "admin" && $password == "1234"){
-    echo "ยินดีต้อนรับเข้าสู่ระบบ";
-  }
-  else {
-    echo "ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด";
-  }
-
+    echo $_GET['data'];   
 ?>
