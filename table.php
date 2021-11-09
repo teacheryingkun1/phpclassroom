e125f622f16959afb28dd248421e068874a79ecf
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-11-09 09:07:54 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,17 @@
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>column 1</td>
+        <td>column 2</td>
+        <td>column 3</td>
+      </tr>
+      <tr>
+        <td>column 1</td>
+        <td>column 2</td>
+        <td>column 3</td>
+      </tr>
+    </table>
+  </body>
+</html>
