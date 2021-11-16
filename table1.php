f9ae9664cc10d3504c17d7dcb958740b76d1cea2
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-11-16 09:15:51 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -2,10 +2,9 @@
   <head></head>
   <body>
     <select>
-      <option value="1991">1991</option>
-      <option value="1992">1992</option>
-      <option value="1993">1993</option>
-      <option value="1994">1994</option>
+      <?php
+        echo "<option value='1991'>1991</option>"
+      ?>
     </select> 
   </body>
 </html>
