dab2aa686863609c91421fefc0e509cda16f95cb
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-11-16 08:59:57 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -1,19 +1,7 @@
-<html>
-  <head></head>
-  <body>
-    <table border="1">
-      <tr>
-        <td>column 1</td>
-        <td>column 2</td>
-        <td>column 3</td>
-        <td>column 4</td>
-      </tr>
-      <tr>
-        <td>column 1</td>
-        <td>column 2</td>
-        <td>column 3</td>
-        <td>column 4</td>
-      </tr>
-    </table>
-  </body>
-</html>
+<?php
+
+  for($i=0;$i<10;$i++){
+    echo "HELLO<br>";
+  }
+
+?>
