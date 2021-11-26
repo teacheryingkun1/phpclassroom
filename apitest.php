95c0b4985a7d0ede621530402412e336d5e37397
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-11-26 13:00:19 +0700

    Update apitest.php

--- apitest.php
+++ apitest.php
@@ -1,6 +1,9 @@
 <?php
-  $homepage = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $data = json_decode(file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all'));
+ /*
   echo '<pre>';
   var_dump(json_decode($homepage));
   echo '</pre>';
+*/
+  echo $data[0]["txn_date"];
 ?>
