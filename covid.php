ffa474c1ad8be9fac35cd8ce67054fd8f8798e86
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-11-26 14:35:25 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,7 +1,14 @@
 <?php
   $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
   $data = json_decode($json);
+
+  echo "<table border='1'>";
+
   foreach ( $data[0] as $key => $val ){
-    echo $key.":".$val."<br>";
+    echo "<tr>";
+    echo "<td>".$key."</td><td>".$val."</td>";
+    echo "</tr>";
   }
+
+  echo "</table>";
 ?>
