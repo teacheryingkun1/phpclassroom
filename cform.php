f368a3210d76e7ab622ad4e92cfdded74a00c8f5
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-03 12:32:05 +0700

    Create cform.php

---
+++ cform.php
@@ -0,0 +1,13 @@
+<?php
+
+  $json =  file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  $data = json_decode($json);
+  echo "กรุณาเลือกจังหวัด : ";
+  echo "<select name='province'>"
+  foreach ( $data as $key => $val ){
+    echo "<option>";
+    echo $val->province;
+    echo "</option>";
+  }
+
+?>
