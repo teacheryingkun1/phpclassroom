f3a05946c4b8d728fa59a7edb986ecd080dc87ff
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-03 13:11:43 +0700

    Update apitest_.php

--- apitest_.php
+++ apitest_.php
@@ -3,11 +3,19 @@
   $json =  file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 
-  echo $data[0]->province;
-  
-  /*
-  echo "<pre>";
-  var_dump($data);
-  echo "</pre>";
-  */
+  echo "<table border='1'>";
+
+  echo "<tr>";
+  echo "<td>ลำดับ</td>";
+  echo "<td>จังหวัด</td>";
+  echo "</tr>";
+
+  foreach ( $data as $key => $val ){
+    echo "<tr>";
+    echo "<td>".$key."</td>";
+    echo "<td>".$val->province."</td>";
+    echo "</tr>";
+  }
+
+  echo "</table>";
 ?>
