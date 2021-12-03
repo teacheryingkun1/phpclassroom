92b50adb5a66214c5d963201cdf87dc51a726e6f
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-03 13:02:25 +0700

    Update apitest_.php

--- apitest_.php
+++ apitest_.php
@@ -3,8 +3,11 @@
   $json =  file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 
+  echo $data[0]->province;
+  
+  /*
   echo "<pre>";
   var_dump($data);
   echo "</pre>";
-
+  */
 ?>
