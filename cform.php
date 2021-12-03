8643295370aeb6c678c42573705faf427b3f92b1
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-03 12:40:56 +0700

    Update cform.php

--- cform.php
+++ cform.php
@@ -2,25 +2,14 @@
 
   $json =  file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
-  /*
-  echo "<pre>";
-  var_dump($data);
-  echo "</pre>";
-  */
-
   
- // echo "กรุณาเลือกจังหวัด : ";
-  //echo "<select name='province'>"
+  echo "กรุณาเลือกจังหวัด : ";
+  echo "<select name='province'>";
     
   foreach ( $data as $key => $val ){
-   // echo "<option>";
-    //echo $key;
-   // echo "</option>";
-    
-    echo "<pre>";
+    echo "<option>";
     echo $val->province;
-    echo "</pre>";
+    echo "</option>";
   }
- // echo "</select>"
   
 ?>
