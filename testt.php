6071c2553698b1571a2a2c5ab63a0f4230b51c9c
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-03 14:08:47 +0700

    Update testt.php

--- testt.php
+++ testt.php
@@ -2,7 +2,6 @@
  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
  $data = json_decode($json);
  
- echo "<pre>";
- var_dump($data);
- echo "</pre>";
+ echo $data[1]->province;
+
 ?>
