8c91fcf35bcd31340ed920718e7a218f7b3f3d0f
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-07 09:14:31 +0700

    Update capi.php

--- capi.php
+++ capi.php
@@ -1,5 +1,6 @@
 <?php
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
-  echo $data[0]->new_case;
+  echo "ยอดผู้ป่วยวันนี้ : ".$data[0]->new_case."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_case."<br>";
 ?>
