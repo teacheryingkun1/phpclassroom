a12c9e9875fd7c23654e32790ccada240d755442
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-14 09:31:24 +0700

    Update province.php

--- province.php
+++ province.php
@@ -2,6 +2,8 @@
 
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
+  echo "<pre>";
   var_dump($data);
+  echo "</pre>";
     
 ?>
