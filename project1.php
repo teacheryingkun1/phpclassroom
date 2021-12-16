a4dd5a05725600346807ead813e0925bdebbc490
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-16 15:40:21 +0700

    Update project1.php

--- project1.php
+++ project1.php
@@ -4,15 +4,12 @@
 
 <h2>HTML Forms</h2>
 
-<form action="/output.php">
+<form action="output.php">
   Number 1 : <input type="number" id="quantity" name="num1" min="1" max="5">
   <br>
-  Number 2 : <input type="number" id="quantity" name="num2" min="1" max="5>
+  Number 2 : <input type="number" id="quantity" name="num2" min="1" max="5">
   <br>  
-  <input type="submit">
+  <input type="submit" value="ส่งข้อมูล">
 </form>
-
-<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
-
 </body>
 </html>
