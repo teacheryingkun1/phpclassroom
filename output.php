f2caf88fbb63fa9e0200199e97d7ec9acbde4397
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-16 15:38:52 +0700

    Update output.php

--- output.php
+++ output.php
@@ -1,5 +1,11 @@
 <?php
 
-  $number = $_GET['q'];
-  echo $number;
+  $number1 = $_GET['num1'];
+  $number2 = $_GET['num2'];
+  echo a($number1,$number2);
+
+  function a($x,$y){
+    return $x+$y;
+  }
+
 ?>
