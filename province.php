fc95f8cca23ab9f531402d94c8ab73fea760ca00
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-21 08:50:07 +0700

    Update province.php

--- province.php
+++ province.php
@@ -6,7 +6,14 @@
   echo "<table border='1'>";
   echo "<tr><td>ลำดับ</td><td>จังหวัด</td></tr>";
   foreach($data as $key=>$val){
-    echo $val->province."<br>";
+    echo "<tr>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "<td>";
+    echo $val->province;
+    echo "</td>";
+    echo "</tr>";
   }
   echo "</table>"; 
 ?>
