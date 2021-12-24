de8fcbd4b6328365a4fa78582ad295d85f9c1bbc
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-24 15:11:33 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -1,5 +1,5 @@
 <?php
-  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces")
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   var_dump($data);
 ?>
