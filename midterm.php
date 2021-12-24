ac06ce4ca86943a69bd8b837186ada865720d491
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2021-12-24 15:29:30 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -3,10 +3,22 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
+  echo "<table border='1'>";
+  echo "<tr>";
+  echo "<td>"; 
+  echo "ลำดับ";
+  echo "</td>"; 
+  echo "<td>"; 
+  echo "จังหวัด";
+  echo "</td>"; 
+  echo "</tr>";
+    
   foreach($data as $key=>$val){
     echo "ข้อมูลตำแหน่งที่ ".$key." : ";
     echo $val->new_case;
     echo "<br>";
   }
+
+  echo "</table>";
 
 ?>
