2e7d051ff93d78761019af65af4b486a2d992c70
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-04 09:13:08 +0700

    Update front.php

--- front.php
+++ front.php
@@ -9,17 +9,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    
+    <style>
+      #col1{
+        background-color: red;
+      }
+      
+      #col2{
+        background-color: green;
+      }
+      
+      #col3{
+        background-color: blue;
+      }
+    </style>
+    
   </head>
   <body>
     <div class="container">
       <div class="row">
-        <div class="col-4">
+        <div id="col1" class="col-4">
           Column
         </div>
-        <div class="col-4">
+        <div id="col2"  class="col-4">
           Column
         </div>
-        <div class="col-4">
+        <div id="col3"  class="col-4">
           Column
         </div>
       </div>
