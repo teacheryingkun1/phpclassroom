536117dbd05c32a5bfafa607e3bde0bd1c77bc09
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-07 13:58:16 +0700

    Update page.php

--- page.php
+++ page.php
@@ -4,9 +4,22 @@
       body {
         background-color: green;
       }
+      #box1{
+        width: 500px;
+        height: 500px;
+        background-color: red;
+      }
+      .box2{
+        width: 500px;
+        height: 500px;
+        background-color: blue;
+      }
     </style>
   </head>
   <body>
-  
+    <div id="box1">
+    </div>
+    <div class="box2">
+    </div>
   </body>
 </html>
