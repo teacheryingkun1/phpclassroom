63bd64da27bdf9d59f1ceabe50cbe09d0b165b28
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-07 14:16:00 +0700

    Update page.php

--- page.php
+++ page.php
@@ -7,11 +7,40 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <title>Hello, world!</title>]
+    
+    <style>
+      #box1{
+        height: 50px;
+        background-color: red;
+      }
+     #box2{
+        height: 50px;
+        background-color: green;
+      }
+      #box3{
+        height: 50px;
+        background-color: blue;
+      }
+    </style>  
+    
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+    <div class="container">
+      <div class="row">
+        <div class="col" id="box1">
+          Column
+        </div>
+        <div class="col" id="box2">
+          Column
+        </div>
+        <div class="col" id="box3">
+          Column
+        </div>
+      </div>
+    </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
