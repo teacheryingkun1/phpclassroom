8406c90b84aee65b03c849b7c73cc58058e217ad
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-11 08:51:10 +0700

    Create style.php

---
+++ style.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <style>
+      #box1{
+        background-color: red;
+        width: 50px;
+        height: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="box1">
+      
+    </div>
+  </body>
+</html>
