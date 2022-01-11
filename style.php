e93365d5b12168008286e8ddd2d1f1d0863fdea4
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-11 08:55:50 +0700

    Update style.php

--- style.php
+++ style.php
@@ -1,12 +1,6 @@
 <html>
   <head>
-    <style>
-      #box1{
-        background-color: red;
-        width: 50px;
-        height: 50px;
-      }
-    </style>
+   <link rel="stylesheet" href="box.css" type="text/css" />
   </head>
   <body>
     <div id="box1">
