91cdce4d118e1c021c7f9b34f31345af1ec6bab1
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-11 09:14:47 +0700

    Update style.php

--- style.php
+++ style.php
@@ -7,6 +7,7 @@
      #box1{
         width:50px;
         height:50px;
+        margin-bottom: 10px;
      }
      #box2{
         width:100px;
@@ -16,7 +17,6 @@
   </head>
   <body>
     <div class="g" id="box1"></div>
-    <br><br><br><br>
     <div  class="g" id="box2"> </div>
   </body>
 </html>
