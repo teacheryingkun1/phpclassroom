c49edc0cbcf63d286a67afe658c112b6a17f48a8
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-14 14:02:29 +0700

    Update gifted.php

--- gifted.php
+++ gifted.php
@@ -1,9 +1,13 @@
 <html>
 <head>
   <title> G I F T E D </title>
-  <link rel="stylesheet" href="mystyle.css" type="text/css">
+  <style>
+    .s1{
+      color:yellow;
+    }
+  </style>
 </head>
 <body>
-  <h1> ยินดีต้อนรับสู่ห้อง GIFTED </h1>
+  <h1 class="s1"> ยินดีต้อนรับสู่ห้อง GIFTED </h1>
 </body>
 </html>
