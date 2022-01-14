8a8be07b444fe70b89c92cf270965c72ad09fe6d
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-01-14 14:25:27 +0700

    Update gifted.php

--- gifted.php
+++ gifted.php
@@ -3,13 +3,21 @@
   <title> G I F T E D </title>
   <style>
     #box1{
-      width: 150px;
-      height: 300px;
-      background-color: brown;
+      width: 50px;
+      height: 50px;
+    }
+    #box2{
+      width: 200px;
+      height: 50px;
+    }
+    .box{
+      background-color: green;
     }
   </style>
 </head>
 <body>
-  <div id="box1">อยากกินก๋วยเตี๋ยวต้มยำอยุทธยา</div>
+  <div id="box1" class="box"></div>
+  <br><br>
+  <div id="box2" class="box"></div>
 </body>
 </html>
