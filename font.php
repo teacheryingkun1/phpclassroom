5f6a83d12c8262245899c2473ad1487c95b94cda
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-02-04 15:24:19 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,6 +1,15 @@
 <html>
   <head>
     <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(shining.woff);
+      }
+      body{
+        font-family: myFont;
+      }
+    </style>
   </head>
   <body>
     <h1>เว็บไซต์ของฉัน</h1>
