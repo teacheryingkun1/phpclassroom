48769c0501a6e37cee21224a9fbf829c2d2f5583
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-02-04 15:29:44 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,17 +6,24 @@
         font-family: myFont;
         src: url(shining.woff);
       }
+      @font-face{
+        font-family: myFont2;
+        src: url(.....);
+      }
       #font1{
         font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
       }
     </style>
   </head>
   <body>
     <div id="font1">
-      <h1>ฟอนต์แบบที่ 1</h1>
+      <h1>นายยิ่งคุณ รอดทิม เลขที่ 35 ห้อง 5/13</h1>
     </div>
     <div id="font2">
-      <h1>ฟอนต์แบบที่ 2</h1>
+      <h1>นายยิ่งคุณ รอดทิม เลขที่ 35 ห้อง 5/13</h1>
     </div>
   </body>
 </html>
