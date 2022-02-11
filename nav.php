4419d230021ba9ff76a14eba18705d8c68e5b9f4
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-02-11 14:09:30 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -4,16 +4,23 @@
     <style>
       ul {
         list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+      }
+      li {
+        float: left;
       }
       li a {
         display: block;
-        padding: 8px 16px;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
         text-decoration: none;
-        background-color: orange;
       }
       li a:hover {
-        background-color: #555;
-        color: white;
+        background-color: #111;
       }
     </style>
   </head>
