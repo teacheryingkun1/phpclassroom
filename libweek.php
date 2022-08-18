e56c0e9946e66cac23bc8827a52d91921d62c156
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-08-18 08:43:15 +0700

    Update libweek.php

--- libweek.php
+++ libweek.php
@@ -1,5 +1,10 @@
 <?php
-  $link = array("https://lomsak.ac.th", "https://pks.ac.th", "http://www.wk.ac.th","https://www.lkp.ac.th");
+  $link = array("https://docs.google.com/forms/d/e/1FAIpQLSfljci_TD_0cfZlQn9OP8Ut3ooVPt1f_MBVK_J_jJYY4WJhPQ/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSez9xuKvvFTey-RhVEWwt2VOZUtSn0OVDqd4742LcerNn-Ykw/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLScThcTXLf-4d8QJYQ6n5GMx6R-Wko109CpD_hL75glsczDSNg/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSdUW6uI1Uz7zzhIO7GOTat1b4ZZ5MxvDHgmpVWn9sHmsmx1YA/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLScjpJjl5KHX4tEk2TnCW5al5pyHhEu0TwIcoLtpSA0hjkJMxA/viewform?usp=sf_link",
+"https://docs.google.com/forms/d/e/1FAIpQLSdvhZPeU93Y0OOIu_Rw-IBGt-THGgObbGQgzgJXdYVSobOjYw/viewform?usp=sf_link");
   $ran_link = $link[rand(0,3)];
   header( "location: ".$ran_link);
   exit(0);
