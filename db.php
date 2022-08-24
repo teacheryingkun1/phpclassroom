dff3515b5898c549fc7c961d6ce99223e291e500
teacheryingkun1 <89376197+teacheryingkun1@users.noreply.github.com>
2022-08-24 17:40:59 +0700

    Update db.php

--- db.php
+++ db.php
@@ -17,7 +17,7 @@
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      echo "id: " . $row["id"]. " - Name: " . $row["student_name"]. " " . $row["student_id"]. "<br>";
     }
   } else {
     echo "0 results";
